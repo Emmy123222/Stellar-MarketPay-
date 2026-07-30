cc90cc26eefa902f78f9ff3e50cd8a02d4383ccd
Godfr3y <157382300+Godfrey-Delight@users.noreply.github.com>
2026-07-30 18:09:43 +0100

    devops: add multi-stage Docker build for production frontend image (#967)

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -16,6 +16,7 @@ const contentSecurityPolicy = [
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   reactStrictMode: true,
   assetPrefix: process.env.NEXT_PUBLIC_CDN_URL || '',
   i18n: {
